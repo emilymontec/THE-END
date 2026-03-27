c806f25f131e7863ec6a0cfa6ca824435703a2b1
emilymontec <emilymonterrosa.c@gmail.com>
2026-03-27 04:56:24 -0500

    Preparacion deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // Instancia de Axios configurada para conectar con el backend de Nhost (local o producción)
 export const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   }
 });
 
